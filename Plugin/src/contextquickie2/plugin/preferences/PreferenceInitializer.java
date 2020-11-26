551a95a39c7c1a6098af04af8e92d4172d2846bc
ContextQuickie <ContextQuickie@users.noreply.github.com>
2020-11-26 20:59:20 +0100

    Created preferences classes (WIP)

---
+++ Plugin/src/contextquickie2/plugin/preferences/PreferenceInitializer.java
@@ -0,0 +1,12 @@
+package contextquickie2.plugin.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+
+public class PreferenceInitializer extends AbstractPreferenceInitializer
+{
+
+  @Override
+  public void initializeDefaultPreferences()
+  {
+  }
+}
---
+++ Plugin/src/contextquickie2/plugin/preferences/PreferencePage.java
@@ -0,0 +1,19 @@
+package contextquickie2.plugin.preferences;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage
+{
+
+  @Override
+  protected void createFieldEditors()
+  {
+  }
+
+  @Override
+  public void init(IWorkbench workbench)
+  {
+  }
+}
