3f9f708602171ee5a0373b012995a62ed24d916c
ContextQuickie <ContextQuickie@users.noreply.github.com>
2020-11-27 10:21:18 +0100

    Created preferences classes (WIP)

--- Plugin/src/contextquickie2/plugin/preferences/PreferenceInitializer.java
+++ Plugin/src/contextquickie2/plugin/preferences/PreferenceInitializer.java
@@ -1,12 +1,43 @@
 package contextquickie2.plugin.preferences;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import contextquickie2.plugin.Activator;
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer
 {
+  private static final String PreferencesPrefix = "contextquickie2.plugin.preferences.";
+
+  public static final String PreferenceNameShowWholeMenu = PreferencesPrefix + "ShowWholeMenu";
+
+  public static final String PreferenceNameExtensionPrefix = PreferencesPrefix + "Show.";
+
+  public static final Map<String, String> SupportedMenuExtensions = createSupportedMenuExtensionsMap();
 
   @Override
   public void initializeDefaultPreferences()
   {
+    final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+    store.setDefault(PreferenceNameShowWholeMenu, Boolean.TRUE.toString());
+
+    for (String key : SupportedMenuExtensions.keySet())
+    {
+      store.setDefault(PreferenceNameExtensionPrefix + key, true);
+    }
+  }
+
+  private static Map<String, String> createSupportedMenuExtensionsMap()
+  {
+    Map<String, String> entries = new HashMap<String, String>();
+    entries.put("Beyond Compare", "{57FA2D12-D22D-490A-805A-5CB48E84F12A}");
+    entries.put("Notepad++",      "{B298D29A-A6ED-11DE-BA8C-A68E55D89593}");
+    entries.put("Open With",      "{09799AFB-AD67-11d1-ABCD-00C04FC30936}");
+    entries.put("TortoiseGit",    "{10A0FDD2-B0C0-4CD4-A7AE-E594CE3B91C8}");
+    entries.put("TortoiseSVN",    "{30351349-7B7D-4FCC-81B4-1E394CA267EB}");
+    return entries;
   }
 }
--- Plugin/src/contextquickie2/plugin/preferences/PreferencePage.java
+++ Plugin/src/contextquickie2/plugin/preferences/PreferencePage.java
@@ -1,19 +1,98 @@
 package contextquickie2.plugin.preferences;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
-public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage
-{
+import contextquickie2.plugin.Activator;
 
-  @Override
-  protected void createFieldEditors()
+public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage, IPropertyChangeListener
+{
+  private RadioGroupFieldEditor radioGroupFieldEditor;
+
+  private List<BooleanFieldEditor> booleanFieldEditors = new ArrayList<BooleanFieldEditor>();
+
+  /**
+   * Constructor.
+   */
+  public PreferencePage()
   {
+    super(GRID);
+    setPreferenceStore(Activator.getDefault().getPreferenceStore());
   }
 
   @Override
   public void init(IWorkbench workbench)
   {
+  }
+
+  @Override
+  protected void createFieldEditors()
+  {
+    this.radioGroupFieldEditor = new RadioGroupFieldEditor(
+      PreferenceInitializer.PreferenceNameShowWholeMenu, 
+      "Appearance", 
+      1, 
+      new String[][]
+      {
+        { "Show the whole context menu as submenu of the context menu", Boolean.TRUE.toString() },
+        { "Show only selected items directly in the context menu", Boolean.FALSE.toString() },
+      }, 
+      this.getFieldEditorParent(), 
+      true);
+    this.addField(this.radioGroupFieldEditor);
+
+
+    List<String> extensions = new ArrayList<String>(PreferenceInitializer.SupportedMenuExtensions.keySet());
+    extensions.sort(Comparator.naturalOrder());
+    for (String key : extensions)
+    {
+      BooleanFieldEditor editor = new BooleanFieldEditor(
+          PreferenceInitializer.PreferenceNameExtensionPrefix + key,
+          "Show " + key,
+          getFieldEditorParent());
+      this.booleanFieldEditors.add(editor);
+      this.addField(editor);
+    }
+
+    this.updateBooleanEditorsEnabledState(this.getPreferenceStore().getString(PreferenceInitializer.PreferenceNameShowWholeMenu));
+  }
+
+  @Override
+  public final void propertyChange(final PropertyChangeEvent event)
+  {
+    if (event.getSource().equals(this.radioGroupFieldEditor))
+    {
+      this.updateBooleanEditorsEnabledState(event.getNewValue().toString());
+    }
+  }
+
+  @Override
+  protected void performDefaults()
+  {
+    super.performDefaults();
+    this.updateBooleanEditorsEnabledState(this.getPreferenceStore().getString(PreferenceInitializer.PreferenceNameShowWholeMenu));
+  }
+
+  private void updateBooleanEditorsEnabledState(String value)
+  {
+    boolean extensionsSelecable = true;
+    if (value.equals(Boolean.TRUE.toString()))
+    {
+      extensionsSelecable = false;
+    }
+
+    for (BooleanFieldEditor editor : this.booleanFieldEditors)
+    {
+      editor.setEnabled(extensionsSelecable, getFieldEditorParent());
+    }
   }
 }
