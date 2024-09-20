c42f4229dd4a3b2fe611a30b39e0e891fb87463d
Roman Huber <26764588+alerosmile@users.noreply.github.com>
2024-09-20 12:38:09 +0200

    Add support for Beyond Compare 5.

--- Plugin/src/contextquickie2/plugin/preferences/PreferenceInitializer.java
+++ Plugin/src/contextquickie2/plugin/preferences/PreferenceInitializer.java
@@ -72,14 +72,15 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer
   private static Map<String, String> createSupportedMenuExtensionsMap()
   {
     Map<String, String> entries = new HashMap<String, String>();
-    entries.put("Beyond Compare", "{57FA2D12-D22D-490A-805A-5CB48E84F12A}");
-    entries.put("Notepad++",      "{B298D29A-A6ED-11DE-BA8C-A68E55D89593}");
-    entries.put("Open With",      "{09799AFB-AD67-11d1-ABCD-00C04FC30936}");
-    entries.put("Path Copy Copy", "{82CB99A2-2F18-4D5D-9476-54347E3B6720}");
-    entries.put("TortoiseGit",    "{10A0FDD2-B0C0-4CD4-A7AE-E594CE3B91C8}");
-    entries.put("TortoiseHg",     "{46605027-5B8C-4DCE-BFE0-051B7972D64C}");
-    entries.put("TortoiseSVN",    "{30351349-7B7D-4FCC-81B4-1E394CA267EB}");
-    entries.put("7-Zip",          "{23170F69-40C1-278A-1000-000100020000}");
+    entries.put("Beyond Compare 4", "{57FA2D12-D22D-490A-805A-5CB48E84F12A}");
+    entries.put("Beyond Compare 5", "{812BC6B5-83CF-4AD9-97C1-6C60C8D025C5}");
+    entries.put("Notepad++",        "{B298D29A-A6ED-11DE-BA8C-A68E55D89593}");
+    entries.put("Open With",        "{09799AFB-AD67-11d1-ABCD-00C04FC30936}");
+    entries.put("Path Copy Copy",   "{82CB99A2-2F18-4D5D-9476-54347E3B6720}");
+    entries.put("TortoiseGit",      "{10A0FDD2-B0C0-4CD4-A7AE-E594CE3B91C8}");
+    entries.put("TortoiseHg",       "{46605027-5B8C-4DCE-BFE0-051B7972D64C}");
+    entries.put("TortoiseSVN",      "{30351349-7B7D-4FCC-81B4-1E394CA267EB}");
+    entries.put("7-Zip",            "{23170F69-40C1-278A-1000-000100020000}");
     return entries;
   }
 }
